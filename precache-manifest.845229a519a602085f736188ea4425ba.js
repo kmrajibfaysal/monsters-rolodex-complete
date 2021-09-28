3d435504b1a714981044c994a155805a88625e15
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-09-29 03:47:05 +0600

    Updates

---
+++ precache-manifest.845229a519a602085f736188ea4425ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c246d6ad027e2356c7fa6a996d8e9254",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "571a692afa4373eaafb6",
+    "url": "/monsters-rolodex-complete/static/css/main.11319235.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571a692afa4373eaafb6",
+    "url": "/monsters-rolodex-complete/static/js/main.314a881c.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
